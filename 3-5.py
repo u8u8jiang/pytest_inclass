2c60bed3756f8b0e4338cc0e0dcd5f288ac3a6bc
u8u8jiang <410211332@gms.ndhu.edu.tw>
2021-01-26 11:40:45 +0800

    ex to 3-9

---
+++ 3-5.py
@@ -0,0 +1,7 @@
+a = int(print())
+b = int(print())
+if a*b > 0:
+    print('NO')
+else:
+    print('YES')
+    
---
+++ 3-9.py
@@ -0,0 +1,11 @@
+m = int(input())
+
+if m==1 or m==3 or m== 5 or m==7 or m==8 or m==10 or m==12:
+# m in [1,3,5,7,8,10,12]
+    print(31)
+elif m==4 or m==6 or m== 9 or m==11:
+# m in [4,6,9,11]
+    print(31)
+else:
+    print(28)
+    
