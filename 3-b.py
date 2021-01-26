ffc55777a492a16726d8782e53ee4ed357d8b3f2
u8u8jiang <410211332@gms.ndhu.edu.tw>
2021-01-26 14:05:59 +0800

    ex to 3-m

---
+++ 3-b.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if b==c:
+    print(a)
+elif a=c:
+    print(b)
+else:
+    print(c)
---
+++ 3-m.py
@@ -0,0 +1,10 @@
+x1 = int(input())
+x2 = int(input())
+x3 = int(input())
+x4 = int(input())
+x5 = int(input())
+
+if :
+    print("YES")
+else:
+    print("NO")
